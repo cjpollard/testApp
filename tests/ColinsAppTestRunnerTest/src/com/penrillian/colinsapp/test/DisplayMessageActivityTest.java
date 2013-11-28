a0ea8e738772877fc482f972ee150781149384cb
cp <colin@penrillian.com>
2013-11-28 16:07:18 +0000

    re-added testrunner

---
+++ tests/ColinsAppTestRunnerTest/src/com/penrillian/colinsapp/test/DisplayMessageActivityTest.java
@@ -0,0 +1,22 @@
+package com.penrillian.colinsapp.test;
+
+import android.test.AndroidTestCase;
+
+public class DisplayMessageActivityTest extends AndroidTestCase {
+
+	public DisplayMessageActivityTest() {
+		super();
+	}
+	
+	public void setUp() {
+		
+	}
+	
+	public void tearDown() {
+		
+	}
+	
+	public void testPreconditions() {
+		
+	}
+}
