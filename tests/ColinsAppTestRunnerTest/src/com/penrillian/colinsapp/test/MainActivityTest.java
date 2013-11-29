ce7c989aef91e30e38bb9889801e1e02fe6fadb8
cp <colin@penrillian.com>
2013-11-29 15:13:38 +0000

    first passing tests

--- tests/ColinsAppTestRunnerTest/src/com/penrillian/colinsapp/test/MainActivityTest.java
+++ tests/ColinsAppTestRunnerTest/src/com/penrillian/colinsapp/test/MainActivityTest.java
@@ -1,12 +1,19 @@
 package com.penrillian.colinsapp.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.ViewAsserts;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.penrillian.colinsapp.MainActivity;
+import com.penrillian.colinsapp.R;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 	
 	private MainActivity mMainActivity;
+	private Button mSendButton;
 	
 	public MainActivityTest() {
 		super(MainActivity.class);
@@ -18,6 +25,24 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 		setActivityInitialTouchMode(false);
 		
 		mMainActivity = getActivity();
+		mSendButton = (Button) mMainActivity.findViewById(R.id.send_button);
+	}
+	
+	public void testPreconditions() {
+		assertNotNull("mMainActivity is null", mMainActivity);
+		assertNotNull("mSendButton is null", mSendButton);
+	}
+	
+	public void testSendButton_layout() {
+		final View decorView = mMainActivity.getWindow().getDecorView();
+		
+		ViewAsserts.assertOnScreen(decorView, mSendButton);
+		
+		final ViewGroup.LayoutParams layoutParams = mSendButton.getLayoutParams();
+		
+		assertNotNull(layoutParams);
+		assertEquals(layoutParams.width, WindowManager.LayoutParams.WRAP_CONTENT);
+		assertEquals(layoutParams.height, WindowManager.LayoutParams.WRAP_CONTENT);
 	}
 
 }
